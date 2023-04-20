10e545f74106adde65300adf31704b117562c5fc
Hidélgio Novela <hidelgionovela@gmail.com>
2023-04-20 21:14:30 +0200

    my first commit

---
+++ src/main/java/com/boot/cifras/CifrasApplication.java
@@ -0,0 +1,13 @@
+package com.boot.cifras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CifrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/cifras/CifrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.cifras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
