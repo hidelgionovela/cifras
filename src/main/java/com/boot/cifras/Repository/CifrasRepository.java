f8e959cc61aff8ea063018afaac0fb9c7c2d787e
Hidélgio Novela <hidelgionovela@gmail.com>
2023-04-22 17:33:43 +0200

    Create CifrasRepository.java

---
+++ src/main/java/com/boot/cifras/Repository/CifrasRepository.java
@@ -0,0 +1,12 @@
+package com.boot.cifras.Repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.boot.cifras.Model.Cifras;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface CifrasRepository extends JpaRepository<Cifras, Integer>{
+         
+}
