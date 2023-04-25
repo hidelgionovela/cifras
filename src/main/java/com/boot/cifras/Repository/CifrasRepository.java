fc1a2d587cb43d23934984c319e3716b67e07acb
Hidélgio Novela <hidelgionovela@gmail.com>
2023-04-25 21:56:33 +0200

    Update CifrasRepository.java

--- src/main/java/com/boot/cifras/Repository/CifrasRepository.java
+++ src/main/java/com/boot/cifras/Repository/CifrasRepository.java
@@ -7,6 +7,6 @@ import com.boot.cifras.Model.Cifras;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface CifrasRepository extends JpaRepository<Cifras, Integer>{
-         
+public interface CifrasRepository extends JpaRepository<Cifras, Integer> {
+
 }
