11e76872bb579b87f59516e1e9e8bbce439c7ee7
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-13 22:32:19 +0200

    Update Cifras.java

--- src/main/java/com/boot/cifras/Model/Cifras.java
+++ src/main/java/com/boot/cifras/Model/Cifras.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "critografia")
 public class Cifras extends AbstractEntity<Long> {
 
-         
          @Column(name = "textoSimples", nullable = true, length = 500)
          private String textoSimples;
          @Column(name = "cesar", nullable = true, length = 500)
@@ -19,34 +17,36 @@ public class Cifras extends AbstractEntity<Long> {
          @Column(name = "vigenere", nullable = true, length = 500)
          private String vigenere;
 
-         
-
          public String getCesar() {
                   return cesar;
          }
+
          public void setCesar(String cesar) {
                   this.cesar = cesar;
          }
+
          public String getHill() {
                   return hill;
          }
+
          public void setHill(String hill) {
                   this.hill = hill;
          }
+
          public String getVigenere() {
                   return vigenere;
          }
+
          public void setVigenere(String vigenere) {
                   this.vigenere = vigenere;
          }
+
          public String getTextoSimples() {
                   return textoSimples;
          }
+
          public void setTextoSimples(String textoSimples) {
                   this.textoSimples = textoSimples;
          }
-         
-         
-         
 
 }
