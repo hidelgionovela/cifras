c95b872f0225db70bc143cbc7a14a3a8f0bc112e
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-13 22:31:55 +0200

    Update ColunarController.java

--- src/main/java/com/boot/cifras/Controllers/ColunarController.java
+++ src/main/java/com/boot/cifras/Controllers/ColunarController.java
@@ -14,46 +14,45 @@ import com.boot.cifras.Service.ColunarService;
 @Controller
 public class ColunarController {
 
+        @Autowired
+        private ColunarService cifrasservice;
 
-         @Autowired
-         private ColunarService cifrasservice;
+        @GetMapping("/colunarHome")
+        public String home() {
+                return "colunar/index";
+        }
 
-         @GetMapping("/colunarHome")
-         public String home() {
-                  return "colunar/index";
-         }
+        @PostMapping("/saveColunar")
+        public String add(Colunar p) {
 
-         @PostMapping("/saveColunar")
-         public String add(Colunar p) {
+                String textoClaro = p.getTextoSimples();
+                String chave = p.getChave();
 
-                  String textoClaro = p.getTextoSimples();
-                  String chave = p.getChave();
+                TransposicaoColunar teste = new TransposicaoColunar(chave);
 
-                  TransposicaoColunar teste = new TransposicaoColunar(chave);
-        
-                  String textoSimples = teste.removerEspacos(textoClaro);
-                  System.out.println(textoSimples);
-          //        Todo: A chave nao pode ter letras repetidas.       
-                  
-                  // String result = teste.cifrar(textoSimples, chave);
-                  String result = teste.cifrar(textoSimples, chave);
-                  p.setTextoCifrado(result);
-                  p.setTextoDecifrado(textoSimples);
-                  cifrasservice.save(p);
-                  return "redirect:/resultadoColunar/"+p.getId();
+                String textoSimples = teste.removerEspacos(textoClaro);
+                System.out.println(textoSimples);
+                // Todo: A chave nao pode ter letras repetidas.
 
-         }
+                // String result = teste.cifrar(textoSimples, chave);
+                String result = teste.cifrar(textoSimples, chave);
+                p.setTextoCifrado(result);
+                p.setTextoDecifrado(textoSimples);
+                cifrasservice.save(p);
+                return "redirect:/resultadoColunar/" + p.getId();
 
-         @GetMapping("/resultadoColunar/{id}")
-         public ModelAndView dados(@PathVariable("id") int id, Model model) {
-                  Colunar c = cifrasservice.getCifraById(id);
-                  model.addAttribute("cifra", c);
+        }
 
-                  ModelAndView m = new ModelAndView();
+        @GetMapping("/resultadoColunar/{id}")
+        public ModelAndView dados(@PathVariable("id") int id, Model model) {
+                Colunar c = cifrasservice.getCifraById(id);
+                model.addAttribute("cifra", c);
 
-                  m.setViewName("/colunar/resultado");
+                ModelAndView m = new ModelAndView();
 
-                  return m;
+                m.setViewName("/colunar/resultado");
 
-         }
+                return m;
+
+        }
 }
